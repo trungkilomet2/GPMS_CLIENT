b717210dafbe1ca0a1c63b57998cc9d4020a9a63
tungphamac <tungptde170703@fpt.edu.vn>
2026-03-03 22:48:18 +0700

    add api config

---
+++ src/lib/config.jsx
@@ -0,0 +1,8 @@
+//API Configuration
+const baseUrl = 'http://localhost:5229'; // Development http URL
+// const baseUrl = 'https://localhost:7096'; // Development https URL
+// const baseUrl = ''; // Production URL
+
+export const API_ENDPOINTS = {
+
+}
